51c58d00d12978d1640adcea90e200b12f152d89
Ethan Wright <ewright362@gmail.com>
2017-10-06 08:53:25 -0600

    added auto indent

--- functions.js
+++ functions.js
@@ -98,3 +98,77 @@ function clearNotes(){
 document.addEventListener('DOMContentLoaded', function() {
   loadNotes();
 }, false);
+
+
+
+/* https://stackoverflow.com/questions/5743916/how-to-add-autoindent-to-html-textarea  */
+$("textarea").keydown(function(e)
+{
+    if (e.which == 9) //ASCII tab
+    {
+        e.preventDefault();
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+        var v = $(this).val();
+        if (start == end)
+        {
+            $(this).val(v.slice(0, start) + "    " + v.slice(start));
+            this.selectionStart = start+4;
+            this.selectionEnd = start+4;
+            return;
+        }
+
+        var selectedLines = [];
+        var inSelection = false;
+        var lineNumber = 0;
+        for (var i = 0; i < v.length; i++)
+        {
+            if (i == start)
+            {
+                inSelection = true;
+                selectedLines.push(lineNumber);
+            }
+            if (i >= end)
+                inSelection = false;
+
+            if (v[i] == "\n")
+            {
+                lineNumber++;
+                if (inSelection)
+                    selectedLines.push(lineNumber);
+            }
+        }
+        var lines = v.split("\n");
+        for (var i = 0; i < selectedLines.length; i++)
+        {
+            lines[selectedLines[i]] = "    " + lines[selectedLines[i]];
+        }
+
+        $(this).val(lines.join("\n"));
+    }
+});
+$("textarea").keypress(function(e)
+{
+    if (e.which == 13) // ASCII newline
+    {
+        setTimeout(function(that)
+        {
+            var start = that.selectionStart;
+            var v = $(that).val();
+            var thisLine = "";
+            var indentation = 0;
+            for (var i = start-2; i >= 0 && v[i] != "\n"; i--)
+            {
+                thisLine = v[i] + thisLine;
+            }
+            for (var i = 0; i < thisLine.length && thisLine[i] == " "; i++)
+            {
+
+                indentation++;
+             }
+             $(that).val(v.slice(0, start) + " ".repeat(indentation) + v.slice(start));
+             that.selectionStart = start+indentation;
+             that.selectionEnd = start+indentation;  
+}, 0.01, this);
+     }
+});
